6230e518f4f7333e78df2654ba7098646c8384f7
zxli <zihinlai@163.com>
2020-06-29 17:44:53 +0800

    Remove a few Matlab routines related to curved boundaries.

--- test/matlab/readpp.m
+++
@@ -1,8 +0,0 @@
-function pp = readpp(handle)
-    tmp = fread(handle, 3, 'int');
-    breaks = fread(handle, [tmp(3)+1 1], 'double');
-    coefs = fread(handle, [tmp(2) tmp(1)*tmp(3)], 'double');
-    coefs = coefs';
-    coefs = coefs(:,end:-1:1);
-    pp = ppmak(breaks,coefs,tmp(1));
-end
